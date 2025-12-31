089218ddd893c3e1209f1cac507f9d30776a42eb
ftplimits <ftplimits@gmail.com>
2025-12-30 19:07:55 -0500

    Refactor room management and player handling

--- server.js
+++ server.js
@@ -22,22 +22,77 @@ const rooms = new Map();
 
 io.on('connection', (socket) => {
     console.log('Player connected:', socket.id);
+    let currentRoom = null;
 
     socket.on('join-room', (roomId) => {
+        currentRoom = roomId;
         socket.join(roomId);
         
         if (!rooms.has(roomId)) {
-            rooms.set(roomId, new Set());
+            rooms.set(roomId, {
+                players: new Set(),
+                images: new Map()
+            });
         }
-        rooms.get(roomId).add(socket.id);
+        
+        const room = rooms.get(roomId);
+        room.players.add(socket.id);
 
         // Notify room of new player
         io.to(roomId).emit('player-joined', {
             playerId: socket.id,
-            playerCount: rooms.get(roomId).size
+            playerCount: room.players.size
+        });
+        
+        // Send existing images to new player
+        room.images.forEach((imageData, imageId) => {
+            socket.emit('image-added', imageData);
         });
 
         console.log(`Player ${socket.id} joined room ${roomId}`);
+    });
+
+    // Dice roll handler
+    socket.on('dice-roll', (data) => {
+        if (!currentRoom) return;
+        
+        // Broadcast to all other players in room
+        socket.to(currentRoom).emit('dice-roll', {
+            ...data,
+            playerId: socket.id
+        });
+        
+        console.log(`Player ${socket.id} rolled D${data.sides}: ${data.result}`);
+    });
+
+    // Image upload handler
+    socket.on('add-image', (data) => {
+        if (!currentRoom) return;
+        
+        const room = rooms.get(currentRoom);
+        if (room) {
+            room.images.set(data.id, data);
+        }
+        
+        // Broadcast to all other players in room
+        socket.to(currentRoom).emit('image-added', data);
+        
+        console.log(`Player ${socket.id} added image ${data.id}`);
+    });
+
+    // Image movement handler
+    socket.on('move-image', (data) => {
+        if (!currentRoom) return;
+        
+        const room = rooms.get(currentRoom);
+        if (room && room.images.has(data.id)) {
+            const imageData = room.images.get(data.id);
+            imageData.x = data.x;
+            imageData.y = data.y;
+        }
+        
+        // Broadcast to all other players in room
+        socket.to(currentRoom).emit('image-moved', data);
     });
 
     socket.on('game-state', (data) => {
@@ -47,16 +102,16 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         // Remove player from all rooms
-        rooms.forEach((players, roomId) => {
-            if (players.has(socket.id)) {
-                players.delete(socket.id);
+        rooms.forEach((room, roomId) => {
+            if (room.players && room.players.has(socket.id)) {
+                room.players.delete(socket.id);
                 io.to(roomId).emit('player-left', {
                     playerId: socket.id,
-                    playerCount: players.size
+                    playerCount: room.players.size
                 });
                 
                 // Clean up empty rooms
-                if (players.size === 0) {
+                if (room.players.size === 0) {
                     rooms.delete(roomId);
                 }
             }
